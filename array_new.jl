bbd3d0c7a52c596a06df3420ddec0f0e0960fe56
Katerina <76427845+Katerinajames@users.noreply.github.com>
2025-06-24 18:58:07 +0300

    Add files via upload

---
+++ array_new.jl
@@ -0,0 +1,11 @@
+
+n=[1,2,3,4,56,5,6]
+print(n)
+print("\nElement addition and array printing \n") 
+push!(n,123)
+print(n)
+print("\n Last element deletion and array printing \n") 
+pop!(n)
+print(n)
+
+
